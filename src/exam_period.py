5a13b32a7c77ac2b4e030f17205a671e4d4bede7
Shaked levy <shakedlevy2112@gmail.com>
2026-05-11 17:53:30 +0300

    fix for exam period

--- src/exam_period.py
+++ src/exam_period.py
@@ -10,25 +10,18 @@ class ExamPeriod:
         # This conversion allows other modules to perform date-based logic and comparisons
         self.start_date = datetime.strptime(start_date.strip(), "%d-%m-%Y").date()
         self.end_date = datetime.strptime(end_date.strip(), "%d-%m-%Y").date()
+        self.forbidden_dates = set()
 
     
     #Generates a complete list of every single date within this exam period
     def get_all_dates_in_range(self) -> list:
         all_dates = []
         current = self.start_date
-        #################################################################
-        #
-        #
-        #
-        #need logic for filtering forbidden 
-        #
-        #
-        #
-        #################################################################
 
         #Iterate through the entire range from start_date to end_date
         while current <= self.end_date:
-            all_dates.append(current)
+            if current not in self.forbidden_dates:
+                all_dates.append(current)
             
             #the next calendar day
             current += timedelta(days=1)

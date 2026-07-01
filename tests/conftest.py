e5d2b02c8ea82f1701849fa6031c749928e264bb
hodaya <hodaya91001@gmail.com>
2026-07-01 17:44:49 +0300

    FIX TEST

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@ import os
 import sys
 
 os.environ.setdefault("QT_QPA_PLATFORM", "offscreen")
+if os.name == "nt":
+    os.environ.setdefault("QT_QPA_FONTDIR", r"C:\Windows\Fonts")
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
